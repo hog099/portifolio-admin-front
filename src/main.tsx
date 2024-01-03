9721757f82ca1bee62e069572c624cab4e162d57
Hudson de Oliveira Ginelli <oliveiraginelli@gmail.com>
2024-01-03 17:10:35 -0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
-import { RoutesApp } from "./routes";
+// import { RoutesApp } from "./routes";
 import { Toaster } from "react-hot-toast";
 
 import { AuthProvider } from "@/contexts/authContext";
@@ -9,13 +9,15 @@ import { AuthProvider } from "@/contexts/authContext";
 import "./styles/styles.css";
 import "./styles/tailwind.css";
 import 'react-loading-skeleton/dist/skeleton.css'
+import { Dashboard } from "./screens/Dashboard/Dashboard";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Router>
       <AuthProvider>
         <Toaster />
-        <RoutesApp />
+        {/* <RoutesApp /> */}
+        <Dashboard />
       </AuthProvider>
     </Router>
   </React.StrictMode>
